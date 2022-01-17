210af7796d35f38e0749310d5c517650563791a1
alec <31748545+burturt@users.noreply.github.com>
2022-01-16 19:36:42 -0800

    Initial commit (other commits contained private info locally, so I reset history to publish)

---
+++ main.py
@@ -0,0 +1,142 @@
+from multiprocessing import Pool, cpu_count
+import random
+from datetime import datetime
+from math import ceil
+import settings
+
+def get_antirequests():
+    room_antirequests_sorted = []
+    for request in settings.room_antirequests:
+        if request[0] < request[1]:
+            room_antirequests_sorted.append((request[0], request[1]))
+        else:
+            room_antirequests_sorted.append((request[1], request[0]))
+
+    return room_antirequests_sorted
+
+def main():
+    room_pairs = calculate_pairing_happiness()
+    # Run algorithm 100 times, select best result
+    result = generate_new_rooms(room_pairs)
+    highest_score = result[0]
+    best_arrangement = result[1]
+    with Pool(10) as pool:
+        results = []
+
+        for i in range(settings.number_of_iterations):
+            results.append(pool.apply_async(generate_new_rooms, args=[room_pairs, settings.seeding_random_seed + i if settings.seeding_random_seed != -1 else -1]))
+            #current_score = rooms[0]
+            #if (highest_score < current_score):
+            #    best_arrangement = rooms[1]
+            #    highest_score = current_score
+        pool.close()
+        pool.join()
+    for result in results:
+        final_result = result.get()
+        if highest_score < final_result[0]:
+            highest_score = final_result[0]
+            best_arrangement = final_result[1]
+    fancy_print_rooms(best_arrangement)
+    print(calculate_room_happiness(best_arrangement, room_pairs))
+
+def calculate_pairing_happiness():
+    room_pairs = {}
+    antirequests = get_antirequests()
+    for student, requests in settings.room_requests.items():
+        if not requests:
+            continue;
+        priority_weight = requests[0]
+        ranked_list = requests[1:]
+        for i in range(len(ranked_list)):
+            student1 = student
+            student2 = ranked_list[i]
+            if (student1 > student2):
+                student1, student2 = student2, student1
+            if (student1, student2) in room_pairs:
+                room_pairs[(student1, student2)] += settings.room_pair_weights[i] + priority_weight + settings.mutual_request_bonus
+            else:
+                room_pairs[(student1, student2)] = settings.room_pair_weights[i] + priority_weight
+    print("Students found:")
+    for student1 in get_students():
+        print(student1)
+        for student2 in get_students():
+            if (student1, student2) in antirequests:
+                room_pairs[(student1, student2)] = -100000000
+    return room_pairs
+def generate_new_rooms(room_pairs, custom_random_seed=-1):
+    rooms = generate_random_rooms(custom_random_seed)
+    rooms = swap_brute_force(rooms, room_pairs)
+    score = calculate_room_happiness(rooms, room_pairs)
+    arrangement = rooms
+    return (score, arrangement)
+
+def calculate_room_happiness(rooms, room_pairs):
+    room_happiness = 0
+    for room in rooms:
+        for i in range(len(room)):
+            j = i
+            while (j < len(room)):
+                student1 = room[i]
+                student2 = room[j]
+                # Avoid 2+ empty seats
+                if student1.startswith("ZZEMPTY") and student2.startswith("ZZEMPTY"):
+                    room_happiness -= 1000
+                if (student1 > student2):
+                    student1, student2 = student2, student1
+                room_happiness += room_pairs.get((student1, student2), 0)
+                j += 1
+
+    return room_happiness
+
+def generate_random_rooms(custom_seed=-1):
+    random_rooms = []
+    students = get_students()
+    # Add dummy students as "empty"
+    for i in range(settings.students_per_room - len(students) % settings.students_per_room):
+        students.append("ZZEMPTY" + str(i))
+    # Shuffle at start to spread out "empty" students
+    if not custom_seed == -1:
+        random.seed(custom_seed)
+    random.shuffle(students)
+
+    for i in range(ceil(len(students) / settings.students_per_room)):
+        random_rooms.append(students[i * settings.students_per_room:i * settings.students_per_room + settings.students_per_room])
+
+    return random_rooms
+
+def get_students():
+    return list(settings.room_requests.keys())
+
+def swap_brute_force(rooms, room_pairs):
+    counter = 0
+    changes = True
+    while changes:
+        changes = False
+        for i1 in range(len(rooms)):
+            for i2 in range(settings.students_per_room):
+                for j1 in range(len(rooms)):
+                    for j2 in range(settings.students_per_room):
+                        orig_happiness = calculate_room_happiness(rooms, room_pairs)
+                        rooms[i1][i2], rooms[j1][j2] = rooms[j1][j2], rooms[i1][i2]
+                        new_happiness = calculate_room_happiness(rooms, room_pairs)
+                        counter += 1
+                        if (orig_happiness >= new_happiness):
+                            rooms[i1][i2], rooms[j1][j2] = rooms[j1][j2], rooms[i1][
+                                i2]
+                        else:
+                            changes = True
+    print("Iteration happiness: " + str(calculate_room_happiness(rooms, room_pairs)))
+    return rooms
+
+def fancy_print_rooms(rooms):
+    for room in rooms:
+        print(sorted(room))
+
+if __name__ == "__main__":
+    # Calculate runtime
+    start = datetime.now()
+    print("Started at: " + str(start))
+    main()
+    print("Ended at: " + str(datetime.now()))
+    print("Time to completion: " + str(datetime.now()-start))
+
---
+++ settings.py
@@ -0,0 +1,130 @@
+from multiprocessing import cpu_count
+import csv
+
+# Whether to use CSV (recommended; easier to work with) or manual variables (legacy, not recommended except for testing)
+use_csv_importer = True
+
+# CSV Importer settings:
+
+# Room requests csv file path:
+room_request_csv_path = "requests.csv"
+# See requests_example.csv for how this csv file should be setup
+# The number in the second column represent the "modifier" - higher modifier = higher chance of getting their picks
+# Example: Leadership gets 10, Seniors get 5, everyone else gets 0
+# If you don't want to use this feature, set them all to 0
+# MAKE SURE NAMES IN THE FIRST COLUMN ARE IDENTICAL IN EVERY WAY TO THE NAMES PUT IN THE REQUEST SECTION, OTHERWISE THIS WON'T WORK!
+
+
+# Ignore first row? If your csv has headers, say True
+ignore_first_row = True
+
+# Room antirequests csv file path:
+room_antireqest_csv_path = "antirequests.csv"
+# See antirequests_example.csv for how this csv file should be setup
+
+
+# Manual variable import settings can be found at the bottom of this file
+
+# CPU Cores used
+# This program will use ALL of your CPU power on the machine if allowed.
+# Default: cpu_count()
+# This will use all of your computer's cpu to minimize runtime, but your computer may lag until completion
+# Consider using cpu_count() - 2 if you want to leave it running in the background
+cpu_cores = cpu_count() * 10
+
+# Number of iterations
+# Basically, the program has an element of randomness when it comes to finding optimal rooms
+# The more iterations, the more likely the program will find a more optimal iteration, but the longer it takes
+# The time the program takes to complete is linearly related to the number of iterations, but the number of iterations
+# on average to find a better room increases exponentially with more iterations
+number_of_iterations = 100
+
+# Random seed start
+# Used to feed into the random number generator
+# If you'd like to see consistent results, set this to a specific POSITIVE number like 53
+# If you'd like the computer to do the randomness for you, set this to -1
+seeding_random_seed = -1
+
+# Request "weights"
+# For each "pair", this is how many points the first, second, third, etc request gets.
+# A "no match" weight is -1000000, aka will never match
+room_pair_weights = [150, 25, 16, 14, 12, 10, 8, 6, 4, 2]
+
+# Number of students per room
+students_per_room = 6
+
+# "Bonus" weight to add if rooming request is mutual
+mutual_request_bonus = 20
+
+# Manual variable importer settings:
+
+# Room requests should be in this format:
+"""
+
+manual_room_requests = {
+    "Student name": [10, "Student request 1", "Student request 2", "Student request 3"],
+    "Student 2 name": [5, "Student request 1"],
+    "Student without requests": [0],
+}
+
+"""
+# The number in the first part of non-avoid brackets represent the "modifier" - higher modifier = higher chance of getting their picks
+# Example: Leadership gets 10, Seniors get 5, everyone else gets 0
+# MAKE SURE NAMES IN THE FIRST COLUMN ARE IDENTICAL IN EVERY WAY TO THE NAMES PUT IN THE REQUEST SECTION!
+
+manual_room_requests = {}
+
+# Room "Anti-requests"
+# Simple list of tuples
+# NOTE: the values in the tuple must be sorted in the order of python string comparsion,
+# which is similar but not exactly like alphabetical sorting (it's ASCII sorting)
+# Names should be identical as in above section
+'''manual_room_antirequests = [
+    ("Student 1", "Student 2"),
+    ("Student 1", "Student 3"),
+    ("Student 3", "Student 4"),
+]'''
+manual_room_antirequests = []
+
+# Configuration processing code
+# DO NOT EDIT ANYTHING BELOW THIS LINE
+
+room_requests = {}
+room_antirequests = []
+
+if use_csv_importer:
+    try:
+        with open(room_request_csv_path, newline='') as room_request_csv:
+            room_request_array = list(csv.reader(room_request_csv))
+            if ignore_first_row:
+                del room_request_array[0]
+            for row in room_request_array:
+                try:
+                    room_requests[row[0]] = [float(row[1])] + row[2:]
+                except ValueError:
+                    print(
+                        "The CSV file " + room_request_csv_path + " is not formatted correctly. Make sure the second column is the request weights!")
+    except:
+        print("Error opening file " + room_request_csv_path)
+        exit(1)
+
+    try:
+        with open(room_antireqest_csv_path, newline='') as room_antirequest_csv:
+            room_antirequest_array = list(csv.reader(room_antirequest_csv))
+            for row in room_antirequest_array:
+                try:
+                    student1 = row[0]
+                    student2 = row[1]
+                    if student1 < student2:
+                        room_antirequests.append((student1, student2))
+                    else:
+                        room_antirequests.append((student1, student2))
+                except:
+                    print("Invalid antirequest pair found, skipping")
+                    print(row)
+    except:
+        print("Error opening file " + room_antireqest_csv_path)
+        exit(1)
+
+else:
+    room_requests = manual_room_requests
